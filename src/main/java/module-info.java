b5105217d8374fec9e381a14311c7821befa7bba
Vetsch Jonas <jonas.vetsch@ict-campus.net>
2026-01-21 14:57:33 +0100

    Start work on UserController.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires com.google.gson;
 
     opens net.ictcampus.semodul.anlagendashboard to javafx.fxml;
     exports net.ictcampus.semodul.anlagendashboard;
--- src/main/java/net/ictcampus/semodul/anlagendashboard/user/UserJdbcDao.java
+++ src/main/java/net/ictcampus/semodul/anlagendashboard/user/UserJdbcDao.java
@@ -14,13 +14,14 @@ import java.sql.SQLException;
 public class UserJdbcDao implements UserDao {
 	/**
 	 * Loads userdata selected by its ID into a new UserModel object.
+	 *
 	 * @param id User-ID
 	 * @return UserModel object containing user data or null if user not found
 	 */
 	public UserModel findById(int id) {
 		String sql = "SELECT ID_User, Email, FirstName, LastName, Password FROM user WHERE ID_User = ?";
 		try (Connection con = ConnectionFactory.getInstance().getConnection();
-			 PreparedStatement ps = con.prepareStatement(sql)){
+			 PreparedStatement ps = con.prepareStatement(sql)) {
 
 			ps.setInt(1, id);
 
